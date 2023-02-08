34b54f67a5f56b1099da873dfb3065ae0f6db824
Anushka Desai <anushkadesai077@gmail.com>
2023-02-07 22:07:14 -0500

    added spinner element, changed certain flows

---
+++ assignment01_demo/scraper_goes18.py
@@ -0,0 +1,67 @@
+import os
+import boto3
+import logging
+from dotenv import load_dotenv
+import pandas as pd
+
+#load env variables and change logging level to info
+load_dotenv()
+LOGLEVEL = os.environ.get('LOGLEVEL', 'INFO').upper()
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    level=LOGLEVEL,
+    datefmt='%Y-%m-%d %H:%M:%S',
+    filename='logs.log')
+
+#authenticate S3 client with your user credentials that are stored in your .env config file
+s3client = boto3.client('s3',
+                        region_name='us-east-1',
+                        aws_access_key_id = os.environ.get('AWS_ACCESS_KEY'),
+                        aws_secret_access_key = os.environ.get('AWS_SECRET_KEY')
+                        )
+
+#intialise dictionary to store scrapped data before moving it to a sqllite table
+scrapped_goes18_dict = {
+    'id': [],
+    'product': [],
+    'year': [],
+    'day': [],
+    'hour': []
+}
+
+def scrape_goes18_data():
+    id=1    #for storing as primary key in db
+    logging.info("Scrapping data from GOES18 bucket")
+    prefix = "ABI-L1b-RadC/"    #replace this with user input from streamlit UI with / in end
+    result = s3client.list_objects(Bucket=os.environ.get('GOES18_BUCKET_NAME'), Prefix=prefix, Delimiter='/')
+    #traversing into each subfolder and store the folder names within each
+    for o in result.get('CommonPrefixes'):
+        path = o.get('Prefix').split('/')
+        prefix_2 = prefix + path[-2] + "/"      #new prefix with added subdirectory path
+        sub_folder = s3client.list_objects(Bucket=os.environ.get('GOES18_BUCKET_NAME'), Prefix=prefix_2, Delimiter='/')
+        for p in sub_folder.get('CommonPrefixes'):
+            sub_path = p.get('Prefix').split('/')
+            prefix_3 = prefix_2 + sub_path[-2] + "/"    #new prefix with added subdirectory path
+            sub_sub_folder = s3client.list_objects(Bucket=os.environ.get('GOES18_BUCKET_NAME'), Prefix=prefix_3, Delimiter='/')
+            for q in sub_sub_folder.get('CommonPrefixes'):
+                sub_sub_path = q.get('Prefix').split('/')
+                sub_sub_path = sub_sub_path[:-1]    #remove the filename from the path
+                scrapped_goes18_dict['id'].append(id)   #map all scrapped data into the dict
+                scrapped_goes18_dict['product'].append(sub_sub_path[0])
+                scrapped_goes18_dict['year'].append(sub_sub_path[1])
+                scrapped_goes18_dict['day'].append(sub_sub_path[2])
+                scrapped_goes18_dict['hour'].append(sub_sub_path[3])
+                id+=1
+
+    logging.info("Data scrapped successfully")       
+    scrapped_goes18_df = pd.DataFrame(scrapped_goes18_dict) #final scrapped metadata stored in dataframe
+    #print(scrapped_goes18_df.loc[scrapped_goes18_df['year']=="-"]['day'].unique())
+    return scrapped_goes18_df
+
+def main():
+    metadata_goes18 = scrape_goes18_data()
+
+if __name__ == "__main__":
+    logging.info("GOES18 scraper script starts")
+    main()
+    logging.info("GOES18 scraper script ends")
---
+++ assignment01_demo/scraper_mapdata.py
@@ -0,0 +1,139 @@
+import requests
+import re
+import os
+import logging
+import plotly.graph_objects as go
+
+#change logging level to info
+LOGLEVEL = os.environ.get('LOGLEVEL', 'INFO').upper()
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    level=LOGLEVEL,
+    datefmt='%Y-%m-%d %H:%M:%S',
+    filename='logs.log')
+
+def scrape_nexrad_locations():
+    #initialise containers for relevant data
+    nexrad=[]
+    satellite_metadata = {
+        'ground_station': [],
+        'state': [],
+        'county': [],
+        'latitude': [],
+        'longitude': [],
+        'elevation': []
+    }
+    #url used to scrape NEXRAD satellite location data to plot on map
+    url = "https://www.ncei.noaa.gov/access/homr/file/nexrad-stations.txt"
+
+    try:
+        response = requests.get(url)    #recording the response from the webpage
+        response.raise_for_status()
+    except requests.exceptions.HTTPError as err_http:
+        logging.error("Exited due to HTTP error while accessing URL")
+        raise SystemExit(err_http)
+    except requests.exceptions.ConnectionError as err_conn:
+        logging.error("Exited due to connection error while accessing URL")
+        raise SystemExit(err_conn)
+    except requests.exceptions.Timeout as err_to:
+        logging.error("Exited due to timeout error while accessing URL")
+        raise SystemExit(err_to)
+    except requests.exceptions.RequestException as err_req:
+        logging.fatal("Exited due to fatal error")
+        raise SystemExit(err_req)
+
+    #traverse extracted txt data line by line
+    lines = response.text.split('\n')
+    for line in lines:
+        line=line.strip()   #strip leading and trailing whitespaces
+        word_list = line.split(" ")
+        if (word_list[-1].upper() == 'NEXRAD'):     #check if the data belongs to a NEXRAD satellite
+            nexrad.append(line)
+    nexrad = [i for i in nexrad if 'UNITED STATES' in i]    #only consider satellites over USA
+
+    #extracting state and county information 
+    for satellite in nexrad:
+        satellite = satellite.split("  ")
+        satellite =  [i.strip() for i in satellite if i != ""]
+        #print(satellite)
+        satellite_metadata['ground_station'].append(satellite[0].split(" ")[1])
+        #print(satellite_metadata['ground_station'])
+        for i in range(len(satellite)):
+            if (re.match(r'\b[A-Z][A-Z]\b',satellite[i].strip())):      #use regex to match with the state field containing 2 capital letters
+                satellite_metadata['state'].append(satellite[i][:2])    #append state field to final dict
+                satellite_metadata['county'].append(satellite[i][2:])   #append county field to final dict
+
+    #extracting latitude, longitude and elevation information 
+    for satellite in nexrad:
+        satellite = satellite.split(" ")
+        satellite =  [i.strip() for i in satellite if i != ""]      #strip for any whitespaces if the string is not empty
+        for i in range(len(satellite)):
+            if (re.match(r'^-?[0-9]\d(\.\d+)?$',satellite[i])):     #use regex to match with the coordinate string
+                state_county = satellite[i].split()
+                satellite_metadata['latitude'].append(satellite[i])     #append latitude as string to final dict
+                satellite_metadata['longitude'].append(satellite[i+1])  #append longtidue as string to final dict
+                satellite_metadata['elevation'].append(int(satellite[i+2]))     #append elevation as int to final dict
+                break
+
+    logging.info("Successfully scrapped NEXRAD statellite location data")
+    return satellite_metadata   #this dict has the final scraped data
+
+def plot_nexrad_locations():
+    satellite_metadata = scrape_nexrad_locations()
+    logging.info("Creating plot for NEXRAD statellite locations")
+    #plotting the coordinates extracted on a map
+    hover_text = []
+    for j in range(len(satellite_metadata['county'])):      #building the text to display when hovering over each point on the plot
+        hover_text.append("Station: " + satellite_metadata['ground_station'][j] + " County:" + satellite_metadata['county'][j] + ", " + satellite_metadata['state'][j])
+
+    #use plotly to plot
+    fig = go.Figure(data=go.Scattergeo(
+            lon = satellite_metadata['longitude'],
+            lat = satellite_metadata['latitude'],
+            text = hover_text,
+            marker= {
+                "color": satellite_metadata['elevation'],
+                "colorscale": "Viridis",
+                "colorbar": {
+                    "title": "Elevation"
+                },
+                "size": 14,
+                "symbol": "circle",
+                "line" : {
+                    "color": 'black',
+                    "width": 1
+                }
+            }
+        ))
+
+    #plot layout
+    fig.update_layout(
+            title = 'All NEXRAD satellite locations along with their elevations',
+            geo_scope='usa',
+            mapbox = {
+                    "zoom": 12,
+                    "pitch": 0,
+                    "center": {
+                        "lat": 31.0127195,
+                        "lon": 121.413115
+                    }
+            },
+            font = {
+                "size": 18
+            },
+            autosize= True
+        )
+
+    fig.update_layout(height=700)
+    #fig.show()
+
+    logging.info("Successfully created plot for NEXRAD statellite locations")
+    return fig
+
+def main():
+    map_plot = plot_nexrad_locations()
+
+if __name__ == "__main__":
+    logging.info("NEXRAD statellite map data scraper script starts")
+    main()
+    logging.info("NEXRAD statellite map data scraper script ends")
